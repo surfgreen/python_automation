444127b3c3480028aa41185e68e4e520d671db94
Adam Goss <adamcgoss@protonmail.com>
2021-01-25 11:20:57 -1000

    renamed py_chee.py to week1.py

---
+++ week1.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import netmiko
+
+# device setup dictionaries for lab
+cisco3 = {
+    'comment': 'Cisco IOS-XE',
+    'hostname': 'cisco3.lasthop.io',
+    'snmp_port': 161,
+    'ssh_port': 22,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'ios_xe'
+}
+
+cisco4 = {
+    'comment': 'Cisco IOS-XE',
+    'hostname': 'cisco4.lasthop.io',
+    'snmp_port': 161,
+    'ssh_port': 22,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'ios_xe'
+}
+
+arista1 = {
+    'comment': 'Arista_vEOS_switch',
+    'hostname': 'arista1.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+}
+
+arista2 = {
+    'comment': 'Arista_vEOS_switch',
+    'hostname': 'arista2.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+}
+
+arista3 = {
+    'comment': 'Arista_vEOS_switch',
+    'hostname': 'arista3.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+}
+
+arista4 = {
+    'comment': 'Arista_vEOS_switch',
+    'hostname': 'arista4.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+}
+
+srx2 = {
+    'comment': 'Juniper_SRX',
+    'hostname': 'srx2.lasthop.io',
+    'ssh_port': 22,
+    'netconf_port': 830,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'juniper'
+}
+
+nxos1 = {
+    'comment': 'NX-OSv Switch',
+    'hostname': 'nxos1.lasthop.io',
+    'ssh_port': 22,
+    'nxapi_port': 8443,
+    'username': 'pyclass',
+    'password': '88newclass',
+}
+nxos2 = {
+    'comment': 'NX-OSv Switch',
+    'hostname': 'nxos2.lasthop.io',
+    'ssh_port': 22,
+    'nxapi_port': 8443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'nxos'
+}
+
+
+def simple_connect(device_list):
+    command = input("Enter the command:\n")
+    for device in device_list:
+        net_connect = netmiko.ConnectHandler(**device)
+        output = net_connect.send_command(command)
+        output.append()
+        print(output)
