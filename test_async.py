6f7a6ca646ff0ffdb823b25d73d8c6fd618d6f95
Adam Goss <adamcgoss@protonmail.com>
2021-02-24 13:27:02 -1000

    Added async code to test, renamed hostnames to host, commented out the dictionary key comment.

---
+++ test_async.py
@@ -0,0 +1,219 @@
+import asyncio
+import netmiko
+import getpass
+
+device_dict = {
+'cisco3': {
+    # 'comment': 'Cisco IOS-XE',
+    'host': 'cisco3.lasthop.io',
+    'snmp_port': 161,
+    'ssh_port': 22,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'ios_xe'
+},
+
+'cisco': {
+    # 'comment': 'Cisco IOS-XE',
+    'host': 'cisco4.lasthop.io',
+    'snmp_port': 161,
+    'ssh_port': 22,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'ios_xe'
+},
+
+'arista1': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista1.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+},
+
+'arista2': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista2.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+},
+
+'arista3': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista3.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+},
+
+'arista4': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista4.lasthop.io',
+    'ssh_port': 22,
+    'eapi_port': 443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'arista'
+},
+
+'srx2': {
+    # 'comment': 'Juniper_SRX',
+    'host': 'srx2.lasthop.io',
+    'ssh_port': 22,
+    'netconf_port': 830,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'juniper'
+},
+
+'nxos1': {
+    # 'comment': 'NX-OSv Switch',
+    'host': 'nxos1.lasthop.io',
+    'ssh_port': 22,
+    'nxapi_port': 8443,
+    'username': 'pyclass',
+    'password': '88newclass',
+},
+
+'nxos2': {
+    # 'comment': 'NX-OSv Switch',
+    'host': 'nxos2.lasthop.io',
+    'ssh_port': 22,
+    'nxapi_port': 8443,
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'nxos'
+}
+}
+
+exec_send_args = {'command_string': "show run",
+                  'expect_string': None,
+                  'delay_factor': 1,
+                  'max_loops': 500,
+                  'auto_find_prompt': True,
+                  'strip_prompt': True,
+                  'strip_command': True,
+                  'normalize': True,
+                  'use_textfsm': False,
+                  'textfsm_template': None,
+                  'use_ttp': False,
+                  'ttp_template': None,
+                  'use_genie': False,
+                  'cmd_verify': True
+                  }
+
+# connection dictionary argument setup functions
+def binary_prompt(user_text):
+    # handles yes and no prompts
+    answer = input(user_text)
+    answer.strip()
+    answer.lower()
+    if answer == 'yes':
+        return True
+    elif answer == 'no':
+        return False
+    else:
+        print("Please enter a valid response. yes or no\n")
+        return binary_prompt(user_text)
+
+
+def filepath_prompt(user_text):
+    # prompts user for filepath and tests if it exists.
+    # Returns [filepath, filename, and boolean of the files exists]
+    # True file exists
+    # False file doesn't exist
+    file_path = input(user_text)
+    # extract filename out of filepath
+    filename_split = file_path.split('/')
+    filename_split_2 = filename_split[-1].split('\\')
+    try:
+        f = open(file_path, 'r')
+        f.close()
+        return [file_path, filename_split_2[-1], True]
+    except FileNotFoundError:
+        return [file_path, filename_split_2[-1], False]
+
+
+def prompt_credentials():
+    # prompts user for username and password and returns [username, password]
+    username = input("Username:\n")
+    prompt = binary_prompt("Use {} as your username? yes or no\n".format(username))
+    if prompt:
+        pass
+    else:
+        return prompt_credentials()
+    password = getpass()
+    print("To prevent lockouts please re-enter your password\n")
+    verify_password = getpass()
+    if password == verify_password:
+        return [username, password]
+    else:
+        print("Passwords do not match\n")
+        return prompt_credentials()
+
+
+async def ssh_connect(sort_dict=device_dict):
+    # takes in a sorted connection dictionary used to make ssh connections
+    # returns our "thread_dict" with the format {device_name: connection_object}
+    device_list = list(sort_dict)
+    coroutine = [ConnectHandler(**sort_dict[device]) for device in device_list]
+    try:
+        threads = await asyncio.gather(*coroutine)
+        thread_dict = {device_list[i]: threads[i] for i in range(len(device_list))}
+        return thread_dict
+    except Exception:  # improve error handling
+        error_code = '''
+        WARNING SOMETHING WENT WRONG
+        Common causes of this are:
+                1. Incorrect hostname or IP address
+                2. Wrong TCP port
+                3. Intermediate firewall blocking access
+                4. Incorrect Credentials
+        Check your settings
+                '''
+        print(error_code)
+        quit()
+
+
+async def ssh_disconnect(thread_dict):
+    # function that disconnects from each device
+    device_list = list(thread_dict)
+    print("Disconnecting from {}".format(device) for device in device_list)
+    coroutine = [thread_dict[device].disconnect() for device in device_list]
+    await asyncio.gather(*coroutine)
+
+
+# async functions that interact with remote devices
+async def send_exec_command(thread_dict, send_dict):
+    # sends the command to each device
+    # thread_dict is a dictionary containing ssh_connection object for its respective device
+    device_list = list(thread_dict)
+    coroutine = [thread_dict[device].send_command(**send_dict) for device in device_list]
+    output_list = await asyncio.gather(*coroutine)
+    output_dict = {device_list[i]: output_list[i] for i in range(len(device_list))}
+    return output_dict
+
+
+def main():
+    # set up our send arguments in here:
+
+    # ssh setup
+    # note need to run asyncio.run(function())
+    thread_dict = asyncio.run(ssh_connect(sorted_dict=device_dict))
+
+    # send commands
+    output = send_exec_command(thread_dict=thread_dict, send_dict_args=exec_send_args)
+    print(output)
+    # close ssh connection
+    ssh_disconnect(thread_dict)
+    return None
+
+if __name__ == "__main__":
+    main()
--- week1.py
+++ week1.py
@@ -3,99 +3,93 @@
 import netmiko
 
 # device setup dictionaries for lab
-cisco3 = {
-    'comment': 'Cisco IOS-XE',
-    'hostname': 'cisco3.lasthop.io',
+device_dict = {
+'cisco3': {
+    # 'comment': 'Cisco IOS-XE',
+    'host': 'cisco3.lasthop.io',
     'snmp_port': 161,
     'ssh_port': 22,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'ios_xe'
-}
+},
 
-cisco4 = {
-    'comment': 'Cisco IOS-XE',
-    'hostname': 'cisco4.lasthop.io',
+'cisco': {
+    # 'comment': 'Cisco IOS-XE',
+    'host': 'cisco4.lasthop.io',
     'snmp_port': 161,
     'ssh_port': 22,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'ios_xe'
-}
+},
 
-arista1 = {
-    'comment': 'Arista_vEOS_switch',
-    'hostname': 'arista1.lasthop.io',
+'arista1': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista1.lasthop.io',
     'ssh_port': 22,
     'eapi_port': 443,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'arista'
-}
+},
 
-arista2 = {
-    'comment': 'Arista_vEOS_switch',
-    'hostname': 'arista2.lasthop.io',
+'arista2': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista2.lasthop.io',
     'ssh_port': 22,
     'eapi_port': 443,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'arista'
-}
+},
 
-arista3 = {
-    'comment': 'Arista_vEOS_switch',
-    'hostname': 'arista3.lasthop.io',
+'arista3': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista3.lasthop.io',
     'ssh_port': 22,
     'eapi_port': 443,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'arista'
-}
+},
 
-arista4 = {
-    'comment': 'Arista_vEOS_switch',
-    'hostname': 'arista4.lasthop.io',
+'arista4': {
+    # 'comment': 'Arista_vEOS_switch',
+    'host': 'arista4.lasthop.io',
     'ssh_port': 22,
     'eapi_port': 443,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'arista'
-}
+},
 
-srx2 = {
-    'comment': 'Juniper_SRX',
-    'hostname': 'srx2.lasthop.io',
+'srx2': {
+    # 'comment': 'Juniper_SRX',
+    'host': 'srx2.lasthop.io',
     'ssh_port': 22,
     'netconf_port': 830,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'juniper'
-}
+},
 
-nxos1 = {
-    'comment': 'NX-OSv Switch',
-    'hostname': 'nxos1.lasthop.io',
+'nxos1': {
+    # 'comment': 'NX-OSv Switch',
+    'host': 'nxos1.lasthop.io',
     'ssh_port': 22,
     'nxapi_port': 8443,
     'username': 'pyclass',
     'password': '88newclass',
-}
-nxos2 = {
-    'comment': 'NX-OSv Switch',
-    'hostname': 'nxos2.lasthop.io',
+},
+
+'nxos2': {
+    # 'comment': 'NX-OSv Switch',
+    'host': 'nxos2.lasthop.io',
     'ssh_port': 22,
     'nxapi_port': 8443,
     'username': 'pyclass',
     'password': '88newclass',
     'device_type': 'nxos'
 }
-
-
-def simple_connect(device_list):
-    command = input("Enter the command:\n")
-    for device in device_list:
-        net_connect = netmiko.ConnectHandler(**device)
-        output = net_connect.send_command(command)
-        output.append()
-        print(output)
+}
